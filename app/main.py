c445c7ed06f448d2e348d052a1040c2f9c594bef
Serhii Markusyk <serega.markusik@gmail.com>
2025-05-31 12:46:15 +0200

    person class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
+    people = {}
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
     pass
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
+def create_person_list(people_l: list) -> list:
+    person_instances = []
+    for person in people_l:
+        person_n = Person(person["name"], person["age"])
+        person_instances.append(person_n)
+    for person in people_l:
+        person_name = Person.people[person["name"]]
+        if "wife" in person:
+            spouse_k = "wife"
+        else:
+            spouse_k = "husband"
+        spouse_name = person.get(spouse_k)
+        if spouse_name:
+            setattr(person_name, spouse_k, Person.people[spouse_name])
+
+    return person_instances
+
+
     pass
